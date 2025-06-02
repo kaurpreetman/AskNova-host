926ee4ae3ccdb4cdf04b6bf333989b3344ae0f52
Manpreet Kaur <preetkaurpawar8@gmail.com>
2025-06-02 22:50:08 +0530

    github callback uri updated

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ import User from '../models/User.js';
 passport.use(new GitHubStrategy({
   clientID: `${process.env.GITHUB_CLIENT_ID}`,
   clientSecret: `${process.env.GITHUB_CLIENT_SECRET}`,
-  callbackURL: '/auth/github/callback'
+  callbackURL: `${process.env.GITHUB_CALLBACK_URL}`
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.emails && profile.emails.length > 0 ? profile.emails[0].value : '';
